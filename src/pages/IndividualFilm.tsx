57b78f524cb096b73363b7fdce6361ec40910c10
Xz33M Hask <pecevgligor@protonmail.com>
2022-09-02 16:21:39 +0200

    display plot from backend api

--- src/pages/IndividualFilm.tsx
+++ src/pages/IndividualFilm.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams, useLocation } from "react-router-dom"
+import { fetcher } from "../lib/api"
 
 function IndividualFilm() {
+    const [filmPlot, setFilmPlot] = useState(null)
     const { title } = useParams()
     const location = useLocation()
     const id = location.state
@@ -9,11 +11,23 @@ function IndividualFilm() {
     useEffect(() => {
         console.log(title, "am title")
         console.log(id, "am id")
-    })
+        fetchData()
+    }, [filmPlot])
+
+    async function fetchData() {
+        const data = await fetcher(`${import.meta.env.VITE_BASE_URL}films/${id}`)
+        console.log(data)
+        setFilmPlot(data.data.attributes.plot)
+    }
 
     return (
         <div>
-            <h1>Hi</h1>
+            <h1>{title}</h1>
+            <br />
+            <br />
+            <br />
+            <h3>Movie plot:</h3>
+            <p>{filmPlot}</p>
         </div>
     )
 }
