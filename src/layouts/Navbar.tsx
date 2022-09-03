f81198f116433b9146bb24edfcf1ee9145f41600
Xz33M Hask <pecevgligor@protonmail.com>
2022-09-03 10:18:56 +0200

    add logout functionality

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,20 +1,20 @@
 import React, { useContext } from "react"
 import { GlobalData } from "../state"
 import Button from "@mui/material/Button"
+import { unsetToken } from "../lib/auth"
 
 export default function Navbar(props: any) {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
     const open = Boolean(anchorEl)
 
     const { loginModal, setLoginModal } = useContext(GlobalData)
+    const logoutUser = () => unsetToken()
 
     return (
         <div className="w-full p-4 flex justify-end max-w-[1350px] mx-auto">
             <Button
                 id="demo-customized-button"
-                aria-controls={open ? "demo-customized-menu" : undefined}
                 aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
                 variant="outlined"
                 disableElevation
                 onClick={() => setLoginModal(true)}
@@ -24,14 +24,15 @@ export default function Navbar(props: any) {
             </Button>
             <Button
                 id="demo-customized-button"
-                aria-controls={open ? "demo-customized-menu" : undefined}
                 aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
                 variant="contained"
                 disableElevation
                 // onClick={}
             >
                 Sign in
+            </Button>
+            <Button id="demo-customized-button" aria-haspopup="true" variant="contained" disableElevation sx={{ marginX: "10px" }} onClick={() => logoutUser()}>
+                Logout
             </Button>
         </div>
     )
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie"
 export const setToken = (data) => {
     if (typeof window === "undefined") return
 
-    Cookies.set("jtw", data.jwt)
+    Cookies.set("jwt", data.jwt)
     Cookies.set("username", data.user.username)
     Cookies.set("id", data.user.id)
 
